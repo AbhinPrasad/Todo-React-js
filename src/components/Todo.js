9a7f09d5cca4139e57582155fa24e2e12b259157
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2022-10-15 11:34:24 +0530

    second commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,56 +1,60 @@
-import React, { Component } from 'react'
-import './Todo.css'
+import React, { Component } from "react";
+import "./Todo.css";
 
 export default class Todo extends Component {
+	state = {
+		input: "",
+		items: [],
+	};
 
-    state = {
-        input: "",
-        items: []
-    }
+	handleChange = (event) => {
+		this.setState({
+			input: event.target.value,
+		});
+	};
 
-    handleChange = (event) => {
-        this.setState({
-            input: event.target.value
-        })
-    }
+	storeItems = (event) => {
+		event.preventDefault();
 
-    storeItems = (event) => {
-        event.preventDefault();
-        const { input } = this.state;
-        this.setState({
-            items: [...this.state.items, input],
-            input: ""
-        })
+		const { input } = this.state;
+		this.setState({
+			items: [...this.state.items, input],
+			input: "",
+		});
+	};
 
-    }
+	deleteItem = (key) => {
+		this.setState({
+			items: this.state.items.filter((data, index) => index !== key),
+		});
+	};
 
-    deleteItem = (key) => {
-        this.setState({
-            items: this.state.items.filter((data, index) => index !== key)
-        })
-    }
+	render() {
+		const { input, items } = this.state;
 
-    render() {
-        const { input, items } = this.state
+		return (
+			<div className="todo-container">
+				<form className="input-section" onSubmit={this.storeItems}>
+					<h1>ToDo App</h1>
+					<input
+						type="text"
+						value={input}
+						onChange={this.handleChange}
+						placeholder="Enter Items..."
+					/>
+				</form>
 
-        return (
-            <div className="todo-container">
-                <form className='input-section' onSubmit={this.storeItems}>
-                    <h1>ToDo App</h1>
-                    <input type="text"
-                        value={input}
-                        onChange={this.handleChange}
-                        placeholder='Enter Items...' />
-                </form>
-
-                <ul>
-                    {items.map((data, index) => (
-                        <li key={index}>
-                            {data} <i className="fas fa-trash-alt" onClick={() => this.deleteItem(index)}></i>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+				<ul>
+					{items.map((data, index) => (
+						<li key={index}>
+							{data}{" "}
+							<i
+								className="fas fa-trash-alt"
+								onClick={() => this.deleteItem(index)}></i>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
